fa3a02fc67c15769495b4c7a7b7f4af18e8e98be
Tiago <tiago_orlando@yahoo.com>
2026-05-13 20:18:14 -0300

    feat: implemented fat16 structures

--- src/fs/fat/fat16.h
+++ src/fs/fat/fat16.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 #include "../file.h"
+#include "disk/disk.h"
+#include "disk/streamer.h"
 
 
 #define GUAVAOS_FAT16_SIGNATURE  0x29
@@ -78,7 +80,57 @@ struct fat_directory_item
     uint16_t creation_time;
     uint16_t creation_date;
     uint16_t last_access;
+    uint16_t high_16_bits_first_cluster;
+    uint16_t last_mod_time;
+    uint16_t last_mod_date;
+    uint16_t low_16_bits_first_cluster;
+    uint32_t filesize;
+} __attribute__((packed));
+
+
+struct fat_directory
+{
+    struct fat_directory_item* item;
+    int total;
+    int sector_pos;
+    int ending_sector_pos;
 };
+
+
+struct fat_item
+{
+    union 
+    {
+        struct fat_directory_item* item;
+        struct fat_directory* directory;
+    };
+    
+    FAT_ITEM_TYPE type;
+};
+
+
+struct fat_item_descriptor
+{
+    struct fat_item* item;
+    uint32_t pos;
+};
+
+
+struct fat_private
+{
+    struct fat_h header;
+    struct fat_directory root_directory;
+
+    // Used to stream data clusters
+    struct disk_stream* cluster_read_stream;
+    // used to stream the file allocation table
+    struct disk_stream* fat_read_stream;
+
+
+    // used in situations where we stream the directory
+    struct disk_stream*  directory_stream;
+};
+
 
 struct filesystem* fat16_init();
 
