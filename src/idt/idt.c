2bef9a62ff01379c8c28df34fe6c2bc5a7cec5ce
Tiago <tiago_orlando@yahoo.com>
2026-06-16 19:54:37 -0300

    refactoring idt

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -9,21 +9,23 @@
 struct idt_desc idt_descriptors[GUAVAOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
 
+extern void* interrupt_pointer_table[GUAVAOS_TOTAL_INTERRUPTS];
+
 static ISR80H_COMMAND isr80h_commands[GUAVAOS_MAX_ISR80H_COMMANDS];
 extern void idt_load(struct idtr_desc* ptr);
-extern void int21h();
 extern void no_interrupt();
 extern void isr80h_wrapper();
 
 
-void int21h_handler()
+
+
+void no_interrupt_handler()
 {
-    print("keyboard pressed\n", 6);
     outb(0x20, 0x20);
 }
 
 
-void no_interrupt_handler()
+void interrupt_handler(int interrupt, struct interrupt_frame* frame)
 {
     outb(0x20, 0x20);
 }
@@ -55,11 +57,10 @@ void idt_init()
 
     for (int i  = 0; i < GUAVAOS_TOTAL_INTERRUPTS; i++)
     {
-        idt_set(i, no_interrupt);
+        idt_set(i, interrupt_pointer_table[i]);
     }
 
     idt_set(0, idt_zero);   // set the first interrupt (div by zero)
-    idt_set(0x21, int21h);
     idt_set(0x80, isr80h_wrapper);
 
     // Load the interrupt descriptor table
